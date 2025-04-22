c3a0e2da9ba03deb58af1aa3c944526d30b55d14
Neonauta <torresivan533@gmail.com>
2025-04-21 21:25:51 -0600

    El commit primerizo

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
-//
-// Created by Laptop on 21/04/2025.
-//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void ArregloInversiones() {
+  int n;
+
+  cout << "Ingrese el numero de activos a analizar: "; cin >> n;
+  int activos[n] = {};
+}
+
+//Función para dar lectura completa a un arreglo.
+void PrintArreglo(int arr[], int size) {
+    //int size = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < size ;i++) {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main (int argc, char** argv){
+    ArregloInversiones();
+ }
+
